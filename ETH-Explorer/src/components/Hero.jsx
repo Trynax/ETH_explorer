d7447fc1b8ad805b351821dab0fa8763ee7a72cd
Trynax <kandoso111@gmail.com>
2024-09-15 03:05:26 +0100

    hero

--- ETH-Explorer/src/components/Hero.jsx
+++ ETH-Explorer/src/components/Hero.jsx
@@ -2,10 +2,27 @@ import onchain from "../assets/on_chain.png"
 
 export default function Hero() {
   return (
-    <section className="bg-black relative h-full">
-        <img className="top-[10%] left-10" src={onchain} alt="" />
-        <img className="top-[10%] right-10" src={onchain} alt="" />
+    <section className="bg-black relative h-[500px]">
 
+        <img className="absolute top-[30%] left-44" src={onchain} alt="" />
+        <img className=" absolute top-[30%] right-44" src={onchain} alt="" /> 
+       <div className="absolute  top-44 left-[50%] transform -translate-x-1/2 flex flex-col gap-3">
+                <h1 className="text-6xl text-center text-white">
+                    The Ethereum <br /> Blockchain Explorer
+                </h1>
+                <div className="relative flex w-[700px] bg-white py-6 items-center rounded-lg">
+    <i className="fa-solid fa-magnifying-glass text-xl ml-3"></i>
+    <input
+        type="text"
+        placeholder="Search by Address / Txn Hash / Block / Domain Name"
+        className="w-full ml-4 focus:outline-none text-gray-600"
+    />
+    <button className="bg-customColor py-2 px-6 text-white rounded-full mr-4">
+        Search
+    </button>
+</div>
+
+       </div>
     </section>
   )
 }
